f9cd7aaab309ddc00c54b8e693829d3771acb65d
Mike Stowe <SnoopSqueak@aim.com>
2017-04-25 00:31:14 -0500

    Apply templates to collection.html

---
+++ app/scripts/fixtures.js
@@ -0,0 +1,49 @@
+// Example Album
+ var albumPicasso = {
+     title: 'The Colors',
+     artist: 'Pablo Picasso',
+     label: 'Cubism',
+     year: '1881',
+     albumArtUrl: 'assets/images/album_covers/01.png',
+     songs: [
+         { title: 'Blue', duration: 161.71, audioUrl: 'assets/music/blue' },
+         { title: 'Green', duration: 103.96, audioUrl: 'assets/music/green' },
+         { title: 'Red', duration: 268.45, audioUrl: 'assets/music/red' },
+         { title: 'Pink', duration: 153.14, audioUrl: 'assets/music/pink' },
+         { title: 'Magenta', duration: 374.22, audioUrl: 'assets/music/magenta' }
+     ]
+ };
+ 
+ // Another Example Album
+ var albumMarconi = {
+     title: 'The Telephone',
+     artist: 'Guglielmo Marconi',
+     label: 'EM',
+     year: '1909',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { title: 'Hello, Operator?', duration: '1:01' },
+         { title: 'Ring, ring, ring', duration: '5:01' },
+         { title: 'Fits in your pocket', duration: '3:21'},
+         { title: 'Can you hear me now?', duration: '3:14' },
+         { title: 'Wrong phone number', duration: '2:15'}
+     ]
+ };
+
+ // Another Example Album
+ var albumMstowe = {
+     title: 'Narcissism',
+     artist: 'Mike Stowe',
+     label: 'ME',
+     year: '1991',
+     albumArtUrl: 'assets/images/album_covers/10.png',
+     songs: [
+         { title: 'Birth', duration: '3:42' },
+         { title: 'The perfect baby', duration: '2:45' },
+         { title: 'Early signs of genius', duration: '4:54'},
+         { title: 'Bad boy phase', duration: '0:51' },
+         { title: 'Continued genius', duration: '13:28'},
+         { title: 'Death', duration: '3:19'},
+         { title: 'Legacy', duration: '9:46'}
+     ]
+ };
